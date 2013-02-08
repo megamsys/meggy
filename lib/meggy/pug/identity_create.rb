df8d7d416ec61ce2feea7c5ccd557cf7e1fe5399
Thomas Alrin <alrin@thomas-work.(none)>
2013-02-08 18:58:13 +0530

    load commands

--- lib/meggy/pug/identity_create.rb
+++ lib/meggy/pug/identity_create.rb
@@ -1,8 +1,11 @@
-require 'rubygems'
-require 'mixlib/cli'
+require 'meggy/pug'
 require 'yaml'
-#module Commands
-  class IdentityCreate < Nm::Command
+require 'mixlib/cli'
+class Meggy
+ class Pug
+  class IdentityCreate < Pug
+
+    include Mixlib::CLI
 
 banner "Usage: ruby mycli.rb sub-command (options)"
 
@@ -20,14 +23,16 @@ banner "Usage: ruby mycli.rb sub-command (options)"
 
     def run(args)
 	id_create = IdentityCreate.new
-	#id_create.parse_options
-	#puts id_create.to_yaml
-	puts "Identity Create"
-	puts args
+	id_create.parse_options
+	puts "Action : #{args[0]}"
+	puts "Identity Name : #{args[2]}"
+	puts "Identity Created with name #{args[2]}"
     end
 
   end
-#end
+end
+end
+
 
 
 
--- lib/nm/commands/identity_help.rb
+++
@@ -1,9 +0,0 @@
-class IdentityHelp
- def list
-puts "Megam Usage"
-puts "ruby mycli.rb [option]..[option]"
-		puts "--login [options]		Login"
-		puts "--identity [options]	Create Identity"
-
- end
-end
